4aee4780da377ae624cae142905c0d1eaa262566
Sang Vo <sangvo111@gmail.com>
2019-08-30 10:30:09 +0700

    chapter 12 password reset

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
-  attr_accessor :remember_token, :activation_token
+  attr_accessor :remember_token, :activation_token, :reset_token
 
   validates :name, presence: true, length: {maximum: Settings.max_name}
   validates :email, format: {with: VALID_EMAIL_REGEX},
@@ -16,11 +16,25 @@ class User < ApplicationRecord
   has_secure_password
 
   def activate
-    update_colunms(activated: true, activated_at: Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
   end
 
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
+  end
+
+  def create_reset_digest
+    self.reset_token = User.new_token
+    update_columns(reset_digest: User.digest(reset_token),
+      reset_sent_at: Time.zone.now)
+  end
+
+  def send_password_reset_email
+    UserMailer.password_reset(self).deliver_now
+  end
+
+  def password_reset_expired?
+    reset_sent_at < Settings.hour_expried_reset.hours.ago
   end
 
   # Decrypt a string
